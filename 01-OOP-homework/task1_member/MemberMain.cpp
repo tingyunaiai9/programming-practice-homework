1bfb556ff3d79eb05ddce113dfc415b619aafd57
tingyunaiai9 <2920825585@qq.com>
2024-08-19 14:44:46 +0800

    add readme
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

---
+++ 01-OOP-homework/task1_member/MemberMain.cpp
@@ -0,0 +1,15 @@
+
+
+
+int main()
+{
+	Member newCommers[5] = { Member("Zhang San", 22),
+	Member("Li Si", 19),
+	Member("Wang Wu", 18),
+	Member("Zhao Liu", 24) };
+	for (int i = 0; i < 5; i++)
+	{
+		cout << newCommers[i] << endl;
+	}
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# programming-practice-homework
+
+此仓库用于存储2024夏季学期“程序设计实训”课程作业
