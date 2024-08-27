6d2f4cdf61242e7956ad54e685c2b5ab5cf4cb37
tingyunaiai9 <2920825585@qq.com>
2024-08-27 14:27:25 +0800

    set up ui for task1_edit
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

---
+++ 05-qt2-homework/task1_edit/task1_edit/mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QVBoxLayout>
+#include <QTextEdit>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// bool MainWindow::eventFilter(QObject *obj, QEvent *event)
+// {
+//     if (obj == ui->inputTextEdit) {
+//         if (event->type() == QEvent::KeyPress) {
+//             QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
+//             qDebug() << "Ate key press" << keyEvent->key();
+//             return true;
+//         } else {
+//             return QMainWindow::eventFilter(obj, event);
+//         }
+//     } else {
+//         // pass the event on to the parent class
+//         return QMainWindow::eventFilter(obj, event);
+//     }
+// }
